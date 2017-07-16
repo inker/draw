989f8dd5ade654b536b9b9235881e8d82d1563a9
Anton <anton.veselyev@gmail.com>
2017-07-16 22:06:09 +0300

    removed min-width

--- src/components/BowlsContainer.tsx
+++ src/components/BowlsContainer.tsx
@@ -4,7 +4,6 @@ export default styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
-  min-width: 35%;
   margin: 0px 5px 10px 5px;
 
   @media (max-width: 999px) {
--- src/components/TablesContainer.tsx
+++ src/components/TablesContainer.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const TablesContainer = styled.div`
   display: flex;
   flex-direction: column;
-  min-width: 65%;
   margin: 0px 5px 10px 5px;
 
   @media (max-width: 999px) {
