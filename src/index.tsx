d1cafd5e3e6166a9f0e5debec63dbf1fd4472fb1
inker <anton.veselyev@gmail.com>
2017-07-25 13:24:21 +0300

    revert

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,9 @@ import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import styled from 'styled-components'
 import * as fastclick from 'fastclick'
-import { safari } from 'bowser'
 import 'normalize.css'
 
 import Routes from './routes'
-
-if (safari) {
-  alert('Does not work in Safari, yet. Use Chrome or Firefox.')
-}
 
 const Root = styled.div`
   font-family: Arial, sans-serif;
