86db60a32afc7cd2bafa5a8933bb48c30bf9eb08
Anton <anton.veselyev@gmail.com>
2017-07-28 23:03:32 +0300

    .

--- src/components/GroupsContainer/GroupCell.tsx
+++ src/components/GroupsContainer/GroupCell.tsx
@@ -24,10 +24,9 @@ const Appear = keyframes`
 
 const GroupCell = styled<Props>(CellWithFlag)`
   ${props => props.possible && `
-    background-color: rgba(255, 255, 255, 0.75);
+    background-color: rgba(255, 255, 255, 0.9);
     animation: ${BorderGlow} 1s ease;
-    border-style: double;
-    border-color: #789;
+    box-shadow: 0 0 5px #bcd;
   `}
   ${props => props.picked && `
     animation: ${Appear} 5s normal forwards;
