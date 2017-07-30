538aa32fc23395907b136ad7bdf8da086cda1dfe
Anton <anton.veselyev@gmail.com>
2017-07-30 18:26:14 +0300

    .

--- src/routes/pages.tsx
+++ src/routes/pages.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Helmet, * as helmet from 'react-helmet'
+import Helmet from 'react-helmet'
 import { uniqueId, memoize } from 'lodash'
 
 import CLGS from 'pages/cl/gs'
