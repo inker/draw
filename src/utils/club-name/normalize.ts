179e12ab27498a71069202ad6f4010a42845f92d
Anton <anton.veselyev@gmail.com>
2017-08-25 19:32:08 +0300

    deps

--- src/utils/club-name/normalize.ts
+++ src/utils/club-name/normalize.ts
@@ -4,11 +4,13 @@ import removeDiacritics from '../removeDiacritics'
 const I_RE = /y|(ij)/g
 const KH_RE = /kh/g
 const ZH_RE = /zh/g
+const CZ = /red star/ig
 
-export default memoize(
-  (s: string) => removeDiacritics(s)
-    .toLowerCase()
-    .replace(I_RE, 'i')
-    .replace(KH_RE, 'k')
-    .replace(ZH_RE, 'j'),
-)
+const normalize = (s: string) => removeDiacritics(s)
+  .toLowerCase()
+  .replace(I_RE, 'i')
+  .replace(KH_RE, 'k')
+  .replace(ZH_RE, 'j')
+  .replace(CZ, 'crvena zvezda')
+
+export default memoize(normalize)
