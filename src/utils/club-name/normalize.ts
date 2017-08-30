69432126a6a9a0710bfd0a378228ff90831180af
Anton <anton.veselyev@gmail.com>
2017-08-30 20:54:25 +0300

    deburr

--- src/utils/club-name/normalize.ts
+++ src/utils/club-name/normalize.ts
@@ -1,12 +1,14 @@
-import { memoize } from 'lodash'
-import removeDiacritics from '../removeDiacritics'
+import {
+  deburr,
+  memoize,
+} from 'lodash'
 
 const I_RE = /y|(ij)/g
 const KH_RE = /kh/g
 const ZH_RE = /zh/g
 const CZ = /red star/ig
 
-const normalize = (s: string) => removeDiacritics(s)
+const normalize = (s: string) => deburr(s)
   .toLowerCase()
   .replace(I_RE, 'i')
   .replace(KH_RE, 'k')
--- src/utils/removeDiacritics.ts
+++
@@ -1,4 +0,0 @@
-const DIACRITICS_RE = /[\u0300-\u036f]/g
-
-export default (s: string) =>
-  s.normalize('NFD').replace(DIACRITICS_RE, '')
