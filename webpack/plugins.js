d5cdf16898238bd0cec0a1dabfd941739c126d27
Anton <anton.veselyev@gmail.com>
2017-09-19 20:42:13 +0300

    separator

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -20,8 +20,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
+const SEP_RE = new RegExp(`\\${path.sep}`, 'g')
 const IS_REACT = /node_modules.+?(react|styled)/
-const PAGES_RE = /pages\/(.+?)(index)?\.[jt]sx?/
+const PAGES_RE = /pages[\/\\](.+?)(index)?\.[jt]sx?/
 
 const moduleToFileNames = (module) => {
   if (!module.request || !module.optional) {
@@ -29,7 +30,7 @@ const moduleToFileNames = (module) => {
   }
   const relativePath = path.relative(module.context, module.request)
   const tokens = relativePath.match(PAGES_RE)
-  return tokens && tokens[1].replace(/\//g, '.').slice(0, -1)
+  return tokens && tokens[1].replace(SEP_RE, '.').slice(0, -1)
 }
 
 module.exports = env => [
