f4014f59c4126776366b494e8ba93678cd4dc576
Anton <anton.veselyev@gmail.com>
2017-10-04 00:31:23 +0300

    fix proxy

--- src/utils/proxify.ts
+++ src/utils/proxify.ts
@@ -1,4 +1,4 @@
-const HOST = 'https://proxy-antonv.rhcloud.com'
+const HOST = 'https://heather-guardian.glitch.me'
 
 export default (url: string, encoding: string) =>
   `${HOST}/?url=${encodeURIComponent(url)}${encoding ? `&encoding=${encoding}` : ''}`
