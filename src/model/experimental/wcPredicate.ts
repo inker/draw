3e67755024950ad764f4754714bab8671dc98b8a
Anton <anton.veselyev@gmail.com>
2017-11-28 00:27:57 +0300

    update predicate

--- src/model/experimental/wcPredicate.ts
+++ src/model/experimental/wcPredicate.ts
@@ -10,6 +10,9 @@ export default (picked: Team, groupIndex: number, currentPotIndex: number, group
   const pickedConfederation = picked.confederation
   const isImpossible = pickedConfederation === 'uefa'
     ? countGreaterThan(group, 1, team => team.confederation === pickedConfederation)
-    : group.some(team => team.confederation === pickedConfederation)
+    : (
+      group.some(team => team.confederation === pickedConfederation)
+      || currentPotIndex === 3 && group.every(team => team.confederation !== 'uefa')
+    )
   return !isImpossible
 }
