57743a469131d8947d2273f0747eae01129112ea
Anton <anton.veselyev@gmail.com>
2017-12-30 04:23:40 +0300

    .

--- src/model/extraConstraints.ts
+++ src/model/extraConstraints.ts
@@ -1,8 +1,11 @@
 import Team from './team'
 
-const pickedRu = (otherTeam: Team) => otherTeam.country === 'ua'
-const pickedUa = (otherTeam: Team) => otherTeam.country === 'ru'
+const CODE_RU = 'ru'
+const CODE_UA = 'ua'
+
+const pickedRu = (otherTeam: Team) => otherTeam.country === CODE_UA
+const pickedUa = (otherTeam: Team) => otherTeam.country === CODE_RU
 const pickedDefault = (otherTeam: Team) => false
 
 export default (teamPicked: Team) =>
-  teamPicked.country === 'ru' ? pickedRu : teamPicked.country === 'ua' ? pickedUa : pickedDefault
+  teamPicked.country === CODE_RU ? pickedRu : teamPicked.country === CODE_UA ? pickedUa : pickedDefault
