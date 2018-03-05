343f6dc9a39dedabd8d3570f976126638d3534ae
inker <anton.veselyev@gmail.com>
2018-03-05 23:49:49 +0300

    .

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -76,24 +76,24 @@ module.exports = env => [
   //   }
   // ]),
 
-  env !== 'dev' && new UglifyJsPlugin({
-    uglifyOptions: {
-      compress: {
-        warnings: false,
-        dead_code: true,
-        properties: true,
-        unused: true,
-        join_vars: true,
-      },
-      mangle: {
-        safari10: true,
-      },
-      output: {
-        comments: false,
-      },
-    },
-    // sourceMap: true, // retains sourcemaps for typescript
-  }),
+  // env !== 'dev' && new UglifyJsPlugin({
+  //   uglifyOptions: {
+  //     compress: {
+  //       warnings: false,
+  //       dead_code: true,
+  //       properties: true,
+  //       unused: true,
+  //       join_vars: true,
+  //     },
+  //     mangle: {
+  //       safari10: true,
+  //     },
+  //     output: {
+  //       comments: false,
+  //     },
+  //   },
+  //   // sourceMap: true, // retains sourcemaps for typescript
+  // }),
 
   env === 'analyze' && new BundleAnalyzerPlugin(),
 ].filter(item => item)
