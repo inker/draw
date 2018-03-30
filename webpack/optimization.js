62c9df26a3fa62a6e0acb474ecadf21b9741e194
inker <anton.veselyev@gmail.com>
2018-03-30 21:35:12 +0300

    upd

--- webpack/optimization.js
+++ webpack/optimization.js
@@ -2,5 +2,29 @@ module.exports = dev => ({
   runtimeChunk: true,
   splitChunks: {
     chunks: 'all',
+    cacheGroups: {
+      // vendor: {
+      vendors: {
+        test: /node_modules/,
+        chunks: 'initial',
+        name: 'vendor',
+        priority: -10000,
+        enforce: true,
+      },
+      react: {
+        test: /[\\/]react(-dom)?[\\/]/,
+        chunks: 'initial',
+        name: 'react',
+        enforce: true,
+        reuseExistingChunk: true,
+      },
+      moment: {
+        test: /[\\/]moment(-timezone)?[\\/]/,
+        chunks: 'initial',
+        name: 'moment',
+        enforce: true,
+        reuseExistingChunk: true,
+      },
+    }
   },
 })
