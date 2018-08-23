9fd21cebfb891750dd2c0750310b692f1b4b0dc7
inker <anton.veselyev@gmail.com>
2018-08-23 14:57:03 +0300

    .

--- src/utils/club-name/normalize.ts
+++ src/utils/club-name/normalize.ts
@@ -7,6 +7,7 @@ const I_RE = /y|(ij)/g
 const KH_RE = /kh/g
 const ZH_RE = /zh/g
 const CZ = /red star/ig
+const RENNES = /stade rennais/ig
 
 const normalize = (s: string) => deburr(s)
   .toLowerCase()
@@ -14,5 +15,6 @@ const normalize = (s: string) => deburr(s)
   .replace(KH_RE, 'k')
   .replace(ZH_RE, 'j')
   .replace(CZ, 'crvena zvezda')
+  .replace(RENNES, 'rennes')
 
 export default memoize(normalize)
