8e220a434bc14dfca14be43dc7a19fcb65188855
inker <anton.veselyev@gmail.com>
2019-04-26 22:27:39 +0100

    proxy

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -12,11 +12,15 @@ export default async (url: string, encoding: string) => {
     console.error("you're offline, retrying...")
     await delay(250)
   }
-  const query = `?url=${encodeURIComponent(url)}${encoding ? `&encoding=${encoding}` : ''}`
+  const searchParams = new URLSearchParams()
+  searchParams.set('url', url)
+  if (encoding) {
+    searchParams.set('encoding', encoding)
+  }
   for (const proxy of proxies) {
     const host = `https://${proxy}.glitch.me`
     try {
-      const response = await fetch(`${host}/${query}`)
+      const response = await fetch(`${host}/?${searchParams.toString()}`)
       if (response.status !== 200) {
         throw new Error(`${url}: ${response.status}`)
       }
