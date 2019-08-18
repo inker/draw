4a3f5398b3759ae797e982b2deaf07f8bdc24d8a
inker <anton.veselyev@gmail.com>
2019-08-19 02:26:59 +0300

    update

--- src/utils/prefetchImage.ts
+++ src/utils/prefetchImage.ts
@@ -1,9 +1,15 @@
 import { memoize } from 'lodash'
 
-export default memoize((url: string) => {
-  const img = document.createElement('img')
+function prefetchImage(url: string) {
+  const link = document.createElement('link')
+  link.rel = 'preload'
+  link.as = 'image'
+  link.onerror = console.error
   return new Promise<void>(resolve => {
-    img.onload = resolve as any
-    img.src = url
+    link.onload = resolve as any
+    link.href = url
+    document.head.appendChild(link)
   })
-})
+}
+
+export default memoize(prefetchImage)
