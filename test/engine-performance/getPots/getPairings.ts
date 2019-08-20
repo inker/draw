588891a8ee19bd44b0a96c8d44597fa8512d7636
inker <anton.veselyev@gmail.com>
2019-08-21 02:16:36 +0300

    remove tests

--- test/engine-performance/getPots/getPairings.ts
+++
@@ -1,6 +0,0 @@
-import clPairings from './pairings/cl'
-import elPairings from './pairings/el'
-
-export default async (season: number, tournament: string): Promise<[string, string][]> => {
-  return tournament === 'cl' ? clPairings : tournament === 'el' ? elPairings : null as any
-}
