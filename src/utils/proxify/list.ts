d0befdc27b69e8ae921989d0fc11f321a5bb2812
inker <anton.veselyev@gmail.com>
2019-08-29 10:55:24 +0100

    add more proxies

--- src/utils/proxify/list.ts
+++ src/utils/proxify/list.ts
@@ -1,7 +1,12 @@
 export default [
   'https://alder-wax.glitch.me',
+  'https://brief-trowel.glitch.me',
   'https://button-match.glitch.me',
+  'https://cat-magic.glitch.me',
   'https://conscious-cheek.glitch.me',
   'https://heather-guardian.glitch.me',
+  'https://lavender-flea.glitch.me',
+  'https://mighty-effect.glitch.me',
+  'https://patch-crush.glitch.me',
   'https://trapezoidal-scorpion.glitch.me',
 ] as const
