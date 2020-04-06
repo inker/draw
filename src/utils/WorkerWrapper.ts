f4279d32e13cd2f42be0da2de91f86cd0c8849b0
inker <anton.veselyev@gmail.com>
2020-04-06 18:06:02 +0100

    private fields

--- src/utils/WorkerWrapper.ts
+++ src/utils/WorkerWrapper.ts
@@ -2,38 +2,38 @@ import AsyncManager from 'async-manager-promise'
 import timelimit from 'timelimit'
 
 class WorkerWrapper<Request, Response> {
-  private readonly worker: Worker
-  private readonly asyncManager = new AsyncManager<Response, string>()
-  private readonly timeout?: number
+  readonly #worker: Worker
+  readonly #asyncManager = new AsyncManager<Response, string>()
+  readonly #timeout?: number
 
   constructor(worker: Worker, timeout?: number) {
-    this.timeout = timeout
-    this.worker = worker
-    this.worker.onmessage = (e) => {
+    this.#timeout = timeout
+    this.#worker = worker
+    this.#worker.onmessage = (e) => {
       const {
         messageId,
         data,
       } = e.data
 
-      this.asyncManager.resolve(messageId, data)
+      this.#asyncManager.resolve(messageId, data)
     }
   }
 
   sendAndReceive(msg: Request) {
-    const promise = this.asyncManager.getPromiseWithId(id => {
-      this.worker.postMessage({
+    const promise = this.#asyncManager.getPromiseWithId(id => {
+      this.#worker.postMessage({
         messageId: id,
         data: msg,
       })
     })
 
-    return this.timeout === undefined
+    return this.#timeout === undefined
       ? promise
-      : timelimit(promise, this.timeout)
+      : timelimit(promise, this.#timeout)
   }
 
   terminate() {
-    this.worker.terminate()
+    this.#worker.terminate()
   }
 }
 
