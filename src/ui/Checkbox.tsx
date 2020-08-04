1314a9205e5f1c5a8b22b5cb8650f1da4b0b56e8
inker <anton.veselyev@gmail.com>
2020-08-04 21:56:04 +0100

    checkbox

---
+++ src/ui/Checkbox.tsx
@@ -0,0 +1,51 @@
+import React, {
+  useCallback,
+  memo,
+  FC,
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+} from 'react'
+import styled from 'styled-components'
+
+const Root = styled.label`
+  display: flex;
+  align-items: center;
+  margin-left: 3px;
+  margin-right: 3px;
+`
+
+const ChildrenWrapper = styled.div`
+  margin-left: 0.5rem;
+`
+
+type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+interface Props extends Omit<InputProps, 'onChange'> {
+  onChange: (value: boolean) => void,
+}
+
+const SelectWithHiddenLabel: FC<Props> = ({
+  children,
+  onChange,
+  ...props
+}: Props) => {
+  const rawOnChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.checked)
+  }, [onChange])
+
+  return (
+    <Root>
+      <input
+        type="checkbox"
+        onChange={rawOnChange}
+        {...props}
+      />
+      <ChildrenWrapper>
+        {children}
+      </ChildrenWrapper>
+    </Root>
+  )
+}
+
+export default memo(SelectWithHiddenLabel)
