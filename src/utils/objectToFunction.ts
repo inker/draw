30b49b30d53c627bf1f92fc108f240d4be5a44b8
inker <anton.veselyev@gmail.com>
2020-08-05 01:29:15 +0100

    update

--- src/utils/objectToFunction.ts
+++ src/utils/objectToFunction.ts
@@ -1,2 +1,7 @@
-export default <T>(obj: { [k: string]: T }) =>
-  (key: keyof typeof obj) => obj[key]
+/**
+ * Makes an object callable,
+ * i.e. converts it to a function, so one can use
+ * round braces instead of square brackets
+ */
+export default <T extends {}>(o: T) =>
+  <K extends keyof T>(key: K) => o[key]
