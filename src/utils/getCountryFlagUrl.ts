0f058cb44c0d6500f986302f502d42f6263613a9
inker <anton.veselyev@gmail.com>
2020-08-14 02:08:03 +0100

    by flag

--- src/utils/getCountryFlagUrl.ts
+++ src/utils/getCountryFlagUrl.ts
@@ -7,8 +7,13 @@ import { Country } from 'model/types'
 declare const require: any
 
 const requireFlag = require.context('flag-icon-css/flags/4x3/', false, /\.svg$/)
+const requireAltFlag = require.context('assets/altFlags/', false, /\.svg$/)
 
-const getCountryFlagUrl = (country: Country) =>
-  requireFlag(`./${countries[country].flag}.svg`).default as string
+const getCountryFlagUrl = (country: Country) => {
+  const req = country === 'Belarus'
+    ? requireAltFlag
+    : requireFlag
+  return req(`./${countries[country].flag}.svg`).default as string
+}
 
 export default memoize(getCountryFlagUrl)
