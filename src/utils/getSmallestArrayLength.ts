94da28af1f6ce1e5fb8995a06a18ec758430636f
inker <anton.veselyev@gmail.com>
2020-08-16 14:00:20 +0100

    upd

--- src/utils/getSmallestArrayLength.ts
+++ src/utils/getSmallestArrayLength.ts
@@ -1,2 +1,2 @@
 export default <T>(arrays: readonly (readonly T[])[]) =>
-  arrays.reduce((a, i) => Math.min(i.length, a), Infinity)
+  arrays.reduce((a, i) => Math.min(i.length, a), Number.MAX_SAFE_INTEGER)
