4126a11a505e2cfb7b43574b2c73b925ceb85113
inker <anton.veselyev@gmail.com>
2020-08-21 21:32:37 +0000

    Deploying to gh-pages from  @ 87f4f5bc19c908c0f0d10e7d68d0230d30c2d535 🚀

---
+++ cl-gs-2020-pairings-txt.48e1a7d3aef5bc252ba4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["cl-gs-2020-pairings-txt"],{468:function(n,a,e){"use strict";e.r(a),a.default="Real Madrid\nBarcelona\n\nSevilla\nAtlético\n\nLiverpool\nChelsea\n\nMan City\nMan United\n\nBayern\nDortmund\n\nLeipzig\nMönchengladbach\n\nJuventus\nInternazionale\n\nLazio\nAtalanta\n\nParis\nMarseille\n"}}]);
