f67bf543a42f98e42cc25f4c2290842bb6c0777f
inker <anton.veselyev@gmail.com>
2020-08-27 20:52:23 +0100

    update GitHub buttons component

--- src/routes/Navbar/GitHubButtons.tsx
+++ src/routes/Navbar/GitHubButtons.tsx
@@ -1,8 +1,23 @@
-import React, { memo } from 'react'
+import React, {
+  useContext,
+  memo,
+} from 'react'
+import styled from 'styled-components'
 import GitHubButton from 'react-github-btn'
 
+const Root = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 170px;
+
+  & > * {
+    margin-left: 5px;
+  }
+`
+
 const GitHubButtons = () => (
-  <>
+  <Root>
     <GitHubButton
       href="https://github.com/inker/draw/issues"
       data-icon="octicon-issue-opened"
@@ -19,7 +34,7 @@ const GitHubButtons = () => (
     >
       Star
     </GitHubButton>
-  </>
+  </Root>
 )
 
 export default memo(GitHubButtons)
