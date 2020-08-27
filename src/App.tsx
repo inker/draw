8db952e109291372030495a36fd6f6e0412bf46d
inker <anton.veselyev@gmail.com>
2020-08-27 11:50:40 +0100

    Body styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 
 import usePopup from 'store/usePopup'
 
+import Body from './Body'
 import Popup from './Popup'
 
 const routesPromise = import(/* webpackPreload: true, webpackChunkName: "routes" */ './routes')
@@ -16,8 +17,6 @@ const Routes = lazy(() => routesPromise)
 const Version = lazy(() => versionPromise)
 
 const Root = styled.div`
-  font-family: Tahoma, Arial, sans-serif;
-
   & * {
     box-sizing: border-box;
   }
@@ -35,15 +34,18 @@ const App = () => {
   }, [popup.waiting])
 
   return (
-    <Root>
-      <Popup />
-      <Suspense fallback={false}>
-        <Routes />
-      </Suspense>
-      <Suspense fallback={false}>
-        <Version />
-      </Suspense>
-    </Root>
+    <>
+      <Body />
+      <Root>
+        <Popup />
+        <Suspense fallback={false}>
+          <Routes />
+        </Suspense>
+        <Suspense fallback={false}>
+          <Version />
+        </Suspense>
+      </Root>
+    </>
   )
 }
 
---
+++ src/Body.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+const Body = createGlobalStyle`
+  body {
+    font-family: Tahoma, Arial, sans-serif;
+  }
+`
+
+export default Body
