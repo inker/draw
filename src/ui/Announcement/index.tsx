a0f575dd62cfb16658665dc27242a4fa1516f35c
inker <anton.veselyev@gmail.com>
2020-09-27 21:10:22 +0300

    increase long calculation timeout

--- src/ui/Announcement/index.tsx
+++ src/ui/Announcement/index.tsx
@@ -148,7 +148,7 @@ const Announcement = ({
                   maxNum={10}
                   interval={2000}
                 />
-                <Deferred delay={5000}>
+                <Deferred delay={10000}>
                   <LongCalculation />
                 </Deferred>
               </div>
