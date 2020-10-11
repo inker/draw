4554c7f62b04d3ec40ce0e988696767940aca906
inker <anton.veselyev@gmail.com>
2020-10-11 16:46:38 +0100

    update webpack

--- webpack/optimization.js
+++ webpack/optimization.js
@@ -2,28 +2,30 @@ const TerserWebpackPlugin = require('terser-webpack-plugin')
 
 module.exports = (isDev) => ({
   runtimeChunk: true,
+  moduleIds: 'deterministic',
+  chunkIds: 'deterministic',
   splitChunks: {
     chunks: 'all',
     cacheGroups: {
       // vendor: {
-      vendors: {
+      defaultVendors: {
         test: /node_modules/,
         chunks: 'initial',
-        name: 'vendors',
+        idHint: 'vendors',
         priority: -10000,
         enforce: true,
       },
       react: {
         test: /[/\\]react(-dom)?[/\\]/,
         chunks: 'initial',
-        name: 'react',
+        idHint: 'react',
         enforce: true,
         reuseExistingChunk: true,
       },
       moment: {
         test: /[/\\]moment(-timezone)?[/\\]/,
         chunks: 'initial',
-        name: 'moment',
+        idHint: 'moment',
         enforce: true,
         reuseExistingChunk: true,
       },
