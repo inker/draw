c210c110ac2b60bf403f0fd6882c522fa1cb7c94
inker <anton.veselyev@gmail.com>
2020-12-10 01:08:01 +0300

    update pots

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export default {
   bertHost: 'https://kassiesa.net/uefa',
   defaultTournament: 'cl',
-  defaultStage: 'gs',
+  defaultStage: 'ko',
   currentSeason: {
     wc: 2018,
     uefa: {
       cl: {
         gs: 2020,
-        ko: 2019,
+        ko: 2020,
       },
       el: {
         gs: 2020,
