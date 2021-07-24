941068429a986aa74c86da0ff53a70d12fa18d96
inker <anton.veselyev@gmail.com>
2021-07-24 17:59:16 +0100

    fix glow

--- src/ui/GroupsContainer/GroupCellBase.ts
+++ src/ui/GroupsContainer/GroupCellBase.ts
@@ -32,12 +32,25 @@ const AppearDark = keyframes`
 
 const Possible = css`
   position: relative; /* enables glow */
-  animation: ${props => props.theme.isDarkMode ? PossibleDark : PossibleLight} 1s ease;
   ${props => props.theme.isDarkMode ? css`
     background-color: rgba(255, 255, 255, 0.3);
   ` : css`
     box-shadow: 0 0 5px #6af;
   `}
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    animation: ${props => props.theme.isDarkMode ? PossibleDark : PossibleLight} 1s ease;
+    ${props => props.theme.isDarkMode ? css`
+      background-color: rgba(255, 255, 255, 0.3);
+    ` : css`
+      box-shadow: 0 0 5px #6af;
+    `}
+  }
 `
 
 const Picked = css`
