89cd207a975ec80fdd225636c3c23ad7c4759a3a
inker <anton.veselyev@gmail.com>
2021-08-25 22:19:22 +0100

    flags map

--- src/utils/getCountryFlagUrl.ts
+++ src/utils/getCountryFlagUrl.ts
@@ -9,9 +9,12 @@ declare const require: any
 const requireFlag = require.context('flag-icon-css/flags/4x3/', false, /\.svg$/)
 const requireAltFlag = require.context('assets/altFlags/', false, /\.svg$/)
 
+const flags = {
+  Belarus: requireAltFlag('./bcb.svg'),
+} as const
+
 const getCountryFlagUrl = (country: Country) =>
-  country === 'Belarus'
-    ? requireAltFlag('./bcb.svg') as string
-    : requireFlag(`./${countries[country].flag}.svg`) as string
+  flags[country] as string
+    ?? requireFlag(`./${countries[country].flag}.svg`) as string
 
 export default memoize(getCountryFlagUrl)
