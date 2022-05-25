bec6288640524e2a31fbb933d7eb72a7dd3e4047
inker <anton.veselyev@gmail.com>
2022-05-25 20:34:28 +0100

    update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,23 +1,176 @@
 module.exports = {
+  processors: [
+    'stylelint-processor-styled-components',
+  ],
   extends: [
-    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-standard-vue/scss',
     'stylelint-config-styled-components',
   ],
-  syntax: 'scss',
   plugins: [
+    'stylelint-order',
     'stylelint-scss',
   ],
   rules: {
-    'declaration-empty-line-before': null,
-    'declaration-colon-newline-after': null, // TODO: fix
     'comment-empty-line-before': null,
     'selector-list-comma-newline-after': null,
-    'block-no-empty': null,
     'no-descending-specificity': null,
-    'at-rule-no-unknown': null,
+    'alpha-value-notation': null,
+    'string-quotes': null, // TODO: remove
+    'declaration-block-no-redundant-longhand-properties': null, // TODO: remove
     'scss/at-rule-no-unknown': true,
+    'scss/selector-no-redundant-nesting-selector': true,
     'block-closing-brace-newline-before': 'always',
     'block-opening-brace-newline-after': 'always',
-    'value-keyword-case': null, // TODO: fix
+    'declaration-block-no-duplicate-properties': true,
+    'selector-class-pattern': /^[a-z][\da-z]*((_{1,2}|-{1,2})[\da-z]+)*$/,
+    'value-keyword-case': [
+      'lower',
+      {
+        camelCaseSvgKeywords: true,
+      },
+    ],
+    'property-no-vendor-prefix': [
+      true,
+      {
+        ignoreProperties: [
+          'appearance',
+        ],
+      },
+    ],
+    'value-no-vendor-prefix': [
+      true,
+    ],
+    'at-rule-no-vendor-prefix': [
+      true,
+    ],
+    indentation: [
+      2,
+      {
+        baseIndentLevel: 0,
+      },
+    ],
+    'order/properties-order': [
+      [
+        'all',
+        'content',
+        {
+          groupName: 'Position',
+          properties: [
+            'position',
+            'top',
+            'right',
+            'bottom',
+            'left',
+            'z-index',
+          ],
+        },
+        'display',
+        'vertical-align',
+        {
+          groupName: 'Flexbox',
+          order: 'flexible',
+          properties: [
+            'flex',
+            'flex-grow',
+            'flex-shrink',
+            'flex-basis',
+            'flex-direction',
+            'flex-flow',
+            'flex-wrap',
+            'gap',
+            'row-gap',
+            'column-gap',
+            'align-content',
+            'align-items',
+            'align-self',
+            'justify-content',
+            'justify-items',
+            'justify-self',
+          ],
+        },
+        {
+          groupName: 'Grid',
+          properties: [
+            'grid',
+            'grid-area',
+            'grid-template',
+            'grid-template-areas',
+            'grid-template-rows',
+            'grid-template-columns',
+            'grid-row',
+            'grid-row-start',
+            'grid-row-end',
+            'grid-column',
+            'grid-column-start',
+            'grid-column-end',
+            'grid-auto-rows',
+            'grid-auto-columns',
+            'grid-auto-flow',
+            'grid-gap',
+            'grid-row-gap',
+            'grid-column-gap',
+          ],
+        },
+        'order',
+        {
+          groupName: 'Float',
+          properties: [
+            'float',
+            'clear',
+          ],
+        },
+        {
+          groupName: 'Clipping',
+          properties: [
+            'overflow',
+            'overflow-x',
+            'overflow-y',
+            'overflow-scrolling',
+            'overscroll-behavior',
+            'overscroll-behavior-x',
+            'overscroll-behavior-y',
+            'overscroll-behavior-inline',
+            'overscroll-behavior-block',
+            'clip',
+          ],
+        },
+        {
+          groupName: 'Margin',
+          properties: [
+            'margin',
+            'margin-top',
+            'margin-right',
+            'margin-bottom',
+            'margin-left',
+          ],
+        },
+        'box-sizing',
+        {
+          groupName: 'Dimensions',
+          properties: [
+            'width',
+            'min-width',
+            'max-width',
+            'height',
+            'min-height',
+            'max-height',
+          ],
+        },
+        {
+          groupName: 'Padding',
+          properties: [
+            'padding',
+            'padding-top',
+            'padding-right',
+            'padding-bottom',
+            'padding-left',
+          ],
+        },
+      ],
+      {
+        unspecified: 'bottom',
+      },
+    ],
   },
 }
