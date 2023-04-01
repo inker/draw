a13f64cd9bbf6d6aa46d51bee6e2adf090f8050a
Anton Veselev <anton.veselyev@gmail.com>
2023-04-01 23:20:05 +0100

    useId

--- src/ui/SelectWithHiddenLabel.tsx
+++ src/ui/SelectWithHiddenLabel.tsx
@@ -1,11 +1,10 @@
 import {
   memo,
+  useId,
   DetailedHTMLProps,
   SelectHTMLAttributes,
 } from 'react'
 import styled, { css } from 'styled-components'
-
-import useRandom from 'utils/hooks/useRandom'
 
 const Root = styled.div`
   display: inline;
@@ -50,7 +49,7 @@ function SelectWithHiddenLabel({
   children,
   ...props
 }: Props) {
-  const [id] = useRandom('select-')
+  const id = useId()
 
   return (
     <Root>
