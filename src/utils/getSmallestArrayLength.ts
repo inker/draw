9ab7d370dcec70837827dd23593913658124433c
Anton Veselev <anton.veselyev@gmail.com>
2023-09-01 22:25:24 +0100

    upd

--- src/utils/getSmallestArrayLength.ts
+++ src/utils/getSmallestArrayLength.ts
@@ -1,2 +1,2 @@
-export default <T>(arrays: readonly (readonly T[])[]) =>
+export default (arrays: readonly (readonly any[])[]) =>
   arrays.reduce((a, i) => Math.min(i.length, a), Number.MAX_SAFE_INTEGER)
