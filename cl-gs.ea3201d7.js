8a2fe13fce081fbfddb9cc00de23cb2050615a81
inker <anton.veselyev@gmail.com>
2023-09-17 17:39:10 +0000

    Deploying to gh-pages from @ inker/draw@bbfba25b46220e25912e24c38a5cf8f072024565 🚀

---
+++ cl-gs.ea3201d7.js
@@ -0,0 +1,67 @@
+"use strict";(self.webpackChunkdraw=self.webpackChunkdraw||[]).push([[1739],{45290:(e,r,l)=>{l.r(r),l.d(r,{default:()=>T});var s=l(85893),o=l(67294),t=l(70394),n=l(21225),i=l(80676),a=l(68800),u=l(21487),c=l(27603),d=l(56285),p=l(43761),f=l(88853),m=l(72723),h=l(11726),b=l(80528),x=l(85704),g=l(18753),Z=l(40410),k=l(40726),j=l(613);let w=(0,t.ZP)(j.Z)`
+  &:hover {
+    ${e=>!e.noHover&&"background: radial-gradient(#ccf, #ccf)"}
+  }
+`,$=t.ZP.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+
+  @media (max-width: 999px) {
+    justify-content: center;
+  }
+`,v=(0,o.memo)(function({display:e,displayGroups:r,possibleGroups:l,onPick:t}){let n=(0,o.useCallback)(e=>{let r=e.target,l=+r.dataset.group;if(Number.isNaN(l))throw TypeError(`Incorrect group ball: ${r.dataset.group}`);t(l)},[t]);return(0,s.jsx)($,{children:e&&l?.map(e=>s.jsx(w,{"data-group":e,forceVisible:r,onClick:n,children:k.Z(e)},e))})});var G=l(12365);let y=()=>new Worker(new URL(l.p+l.u(7685),l.b)),C=()=>new Worker(new URL(l.p+l.u(3407),l.b)),P=(0,t.iv)`
+  background-color: ${e=>e.theme.isDarkMode?"#933":"#ffc0c0"};
+`,E=(0,t.iv)`
+  background-color: ${e=>e.theme.isDarkMode?"#039":"#c0e0ff"};
+`;function N(e){let r=e.map(e=>(0,n.Z)(e)),l=r[0];return{currentPotNum:0,selectedTeam:null,pickedGroup:null,hungPot:l,possibleGroups:null,possibleGroupsShuffled:null,pots:r,groups:e[0].map(()=>[])}}let T=(0,o.memo)(function({season:e,pots:r,isFirstPotShortDraw:l}){let[t,k]=(0,c.Z)(),[j]=(0,d.Z)(),[{currentPotNum:w,selectedTeam:$,pickedGroup:T,hungPot:D,possibleGroups:M,possibleGroupsShuffled:S,pots:F,groups:R},A]=(0,o.useState)(()=>N(r));(0,o.useEffect)(()=>{A(N(r))},[r,t]);let[,L]=(0,u.Z)(),[U]=(0,p.Z)(),W=(0,f.Z)(C),H=(0,f.Z)(y),I=(0,o.useRef)(null),V=l&&0===w,q=j||V,z=async()=>{let r;if(!$)throw Error("no selected team");try{if(V){let l=await W({season:e,pots:F,groups:R,selectedTeam:$});r=[l]}else{let l=await H({season:e,pots:F,groups:R,selectedTeam:$});r=l}}catch(e){console.error(e),L({error:"Could not determine the group"});return}A(e=>({...e,pickedGroup:null,possibleGroups:r,possibleGroupsShuffled:(0,n.Z)(r)}))},B=(0,o.useCallback)(e=>{if($)return;let r=F[w],l=r[e];if(!l)return;let s=F.slice();s[w]=s[w].filter((r,l)=>l!==e),A(e=>({...e,selectedTeam:l,pickedGroup:null,pots:s}))},[F,w,$]),J=(0,o.useCallback)(e=>{if(!$){L({error:"No selected team..."});return}let r=R.slice();r[e]=[...r[e],$];let l=F[w].length>0?w:w+1;A(s=>({...s,selectedTeam:null,pickedGroup:e,hungPot:F[l],possibleGroups:null,possibleGroupsShuffled:null,currentPotNum:l,groups:r}))},[F,R,$,w,D]);(0,o.useEffect)(()=>{$&&z()},[$]);let K=w>=F.length;(0,o.useEffect)(()=>{let e=D?.length;if(j&&e){let r=(0,i.Z)(e-1);B(r)}},[j,D]),(0,o.useEffect)(()=>{if(q&&S?.length){let e=V?Math.min(...S):(0,a.Z)(S);J(e)}},[q,S]);let O=R.length,Q=(0,o.useCallback)(e=>e<O>>1?P:E,[O]);return(0,s.jsxs)(m.Z,{children:[(0,s.jsxs)(x.Z,{children:[(0,s.jsx)(h.Z,{selectedTeams:$&&[$],initialPots:r,pots:F,currentPotNum:w}),(0,s.jsx)(b.Z,{ref:I,maxTeams:F.length,currentPotNum:w,groups:R,possibleGroups:q?null:M,getGroupHeaderStyles:Q})]}),(0,s.jsxs)(g.Z,{children:[!j&&(0,s.jsx)(Z.Z,{display:!K,displayTeams:U,selectedTeam:$,pot:D,onPick:B}),(0,s.jsx)(G.Z,{long:!1,completed:K,selectedTeam:$,pickedGroup:T,possibleGroups:j?null:M,isDisplayPossibleGroupsText:!!$,numGroups:O,groupsElement:I,reset:k}),!q&&(0,s.jsx)(v,{display:!K,displayGroups:U,possibleGroups:S,onPick:J})]})]})})},80528:(e,r,l)=>{l.d(r,{Z:()=>P});var s=l(85893),o=l(67294),t=l(70394),n=l(40726),i=l(46202),a=l(48606),u=l(1850),c=l(8360),d=l(90595),p=l(63835),f=l(63494),m=l(79633),h=l(88834),b=l(43955);let x=(0,t.F4)`
+  from {
+    box-shadow: 0 0 20px #08f;
+  }
+`,g=(0,t.F4)`
+  from {
+    background-color: rgb(255 255 255 / 0.5);
+  }
+`,Z=(0,t.F4)`
+  from {
+    background-color: rgb(255 255 0 / 0.5);
+  }
+`,k=(0,t.F4)`
+  from {
+    background-color: rgb(192 224 255 / 0.5);
+  }
+`,j=(0,t.iv)`
+  position: relative; /* enables glow */
+  ${e=>e.theme.isDarkMode?(0,t.iv)`
+      background-color: rgb(255 255 255 / 0.3);
+    `:(0,t.iv)`
+      box-shadow: 0 0 5px #6af;
+    `}
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    animation: ${e=>e.theme.isDarkMode?g:x} 1s ease;
+    ${e=>e.theme.isDarkMode?(0,t.iv)`
+      background-color: rgb(255 255 255 / 0.3);
+    `:(0,t.iv)`
+      box-shadow: 0 0 5px #6af;
+    `}
+  }
+`,w=(0,t.iv)`
+  animation: ${e=>e.theme.isDarkMode?k:Z} 3s ease-out normal forwards;
+`,$=(0,t.ZP)(c.Z)`
+  ${e=>e.$possible&&j}
+  ${e=>e.$picked&&w}
+`,v=(0,o.memo)(function({team:e,possible:r}){let l=(0,d.Z)(e),[n,i]=(0,o.useState)(e),[a,u]=(0,o.useState)(!1),c=(0,o.useContext)(t.Ni),x=(0,o.useRef)(null),g=(0,o.useCallback)(()=>u(!1),[]),Z=(0,o.useCallback)(()=>{i(e),u(!0)},[e]);return(0,p.Z)(()=>{g()},[c]),(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)($,{$picked:a&&!!n,$possible:r,onAnimationEnd:g,children:n?(0,s.jsx)(m.Z,{$country:(0,f.Z)(n),children:n.shortName??n.name}):(0,s.jsx)(h.Z,{ref:x})}),e&&e!==l&&(0,s.jsx)(b.Z,{from:`[data-cellid='${e.id}']`,to:x,duration:350,team:e,onAnimationEnd:Z})]})}),G=(0,o.memo)(function({maxTeams:e,groupLetter:r,teams:l,potNum:o,possible:t,headerStyles:n}){return(0,s.jsxs)(i.Z,{children:[(0,s.jsx)("thead",{children:(0,s.jsx)(u.Z,{children:(0,s.jsx)(c.Z,{children:(0,s.jsxs)(a.Z,{$styles:n,children:["Group"," ",r]})})})}),(0,s.jsx)("tbody",{children:Array.from({length:e},(e,r)=>(0,s.jsx)(u.Z,{children:(0,s.jsx)(v,{team:l[r],possible:r===o&&t})},r))})]})}),y=t.ZP.div`
+  display: flex;
+  flex-flow: row wrap;
+
+  > * {
+    flex: 1;
+    flex-basis: 22%;
+  }
+`,C=(0,o.forwardRef)(({maxTeams:e,currentPotNum:r,groups:l,possibleGroups:o,getGroupHeaderStyles:t},i)=>(0,s.jsx)(y,{ref:i,children:l?.map((l,i)=>{let a=n.Z(i),u=t?.(i);return s.jsx(G,{maxTeams:e,groupLetter:a,teams:l,potNum:r,possible:!!o?.includes(i),headerStyles:u},a)})})),P=(0,o.memo)(C)}}]);
---
+++ el-ko.f5bc7444.js
@@ -0,0 +1,31 @@
+"use strict";(self.webpackChunkdraw=self.webpackChunkdraw||[]).push([[6349],{27045:(e,t,s)=>{s.r(t),s.d(t,{default:()=>y});var n=s(85893),l=s(67294),r=s(21225),i=s(80676),o=s(21487),a=s(27603),u=s(56285),c=s(43761),d=s(12185),m=s(88853),p=s(72723),h=s(11726),Z=s(31294),f=s(85704),x=s(18753),j=s(40410),g=s(98514),b=s(12365);let k=()=>new Worker(new URL(s.p+s.u(7074),s.b));function w(e,t){return{currentMatchupNum:0,currentPotNum:1,possiblePairings:null,pots:e.map(e=>(0,r.Z)(e)),matchups:Array.from({length:t<2021?16:8},()=>[])}}let y=(0,l.memo)(function({season:e,pots:t}){let[s,r]=(0,a.Z)(),[y]=(0,u.Z)(),[{currentMatchupNum:P,currentPotNum:$,possiblePairings:C,pots:N,matchups:v},E]=(0,l.useState)(()=>w(t,e));(0,l.useEffect)(()=>{E(w(t,e))},[t,e,s]);let T=(0,d.Z)("(min-height: 750px)"),[,M]=(0,o.Z)(),[R]=(0,c.Z)(),S=(0,m.Z)(k),F=(0,l.useRef)(null),G=(0,l.useCallback)(async(t,s)=>{try{return await S({season:e,pots:t,matchups:s})}catch(e){throw M({error:"Could not determine possible pairings"}),e}},[e,S]),A=(0,l.useCallback)(async e=>{let t=N[$],s=C?C[e]:e,n=t[s],l=N.slice();l[$]=l[$].filter((e,t)=>t!==s);let r=v.slice();r[P]=[...r[P],n];let i=1===$?await G(l,r):null,o=P-$+1;E(e=>({...e,currentPotNum:1-$,currentMatchupNum:o,possiblePairings:i,pots:l,matchups:r}))},[N,v,$,P,C]),D=()=>{if(y)return;let e=C?.length===1||1===$&&1===N[1].length;e&&A(0)};(0,l.useEffect)(()=>{setTimeout(D,250)},[$]);let W=(0,l.useMemo)(()=>C&&N[0].filter((e,t)=>C.includes(t)),[C]),L=P>=t[0].length;(0,l.useEffect)(()=>{if(y){let e=W??N[1],t=e.length;if(t>0){let e=(0,i.Z)(t-1);A(e)}}},[y,$]);let U=C?C.map(e=>N[0][e]):[];return(0,n.jsxs)(p.Z,{children:[(0,n.jsxs)(f.Z,{children:[(0,n.jsx)(h.Z,{selectedTeams:U,initialPots:t,pots:N,currentPotNum:$,split:!T&&e<2021}),(0,n.jsx)(Z.Z,{ref:F,matchups:v})]}),(0,n.jsxs)(x.Z,{children:[!y&&(0,n.jsxs)(n.Fragment,{children:[!L&&(0,n.jsx)(g.Z,{children:"Runners-up"}),(0,n.jsx)(j.Z,{forceNoSelect:0===$,display:!L,displayTeams:R,selectedTeam:null,pot:N[1],onPick:A}),!L&&(0,n.jsx)(g.Z,{children:"Group Winners"}),W&&(0,n.jsx)(j.Z,{forceNoSelect:1===$,display:!L,displayTeams:R,selectedTeam:null,pot:W,onPick:A})]}),L&&(0,n.jsx)(b.Z,{long:!1,completed:L,selectedTeam:null,pickedGroup:null,possibleGroups:null,numGroups:0,groupsElement:F,reset:r})]})]})})},31294:(e,t,s)=>{s.d(t,{Z:()=>$});var n=s(85893),l=s(67294),r=s(70394),i=s(46202),o=s(1850),a=s(8360),u=s(73995),c=s(90595),d=s(63835),m=s(63494),p=s(79633),h=s(88834),Z=s(43955);let f=(0,r.F4)`
+  from {
+    background-color: rgb(255 255 0 / 0.5);
+  }
+`,x=(0,r.F4)`
+  from {
+    background-color: rgb(192 224 255 / 0.5);
+  }
+`,j=(0,r.iv)`
+  animation: ${e=>e.theme.isDarkMode?x:f} 3s ease-out normal forwards;
+`,g=(0,r.ZP)(a.Z)`
+  width: 150px;
+  ${e=>e.$picked&&j}
+  ${e=>e.$styles}
+`,b=(0,l.memo)(function({team:e}){let t=(0,c.Z)(e),[s,i]=(0,l.useState)(e),[o,a]=(0,l.useState)(!1),u=(0,l.useContext)(r.Ni),f=(0,l.useRef)(null),x=(0,l.useCallback)(()=>a(!1),[]),j=(0,l.useCallback)(()=>{i(e),a(!0)},[e]);return(0,d.Z)(()=>{x()},[u]),(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(g,{$picked:o&&!!s,onAnimationEnd:x,children:s?(0,n.jsx)(p.Z,{$country:(0,m.Z)(s),children:s.shortName??s.name}):(0,n.jsx)(h.Z,{ref:f})}),e&&e!==t&&(0,n.jsx)(Z.Z,{from:`[data-cellid='${e.id}']`,to:f,duration:350,team:e,onAnimationEnd:j})]})}),k=(0,r.ZP)(u.Z)`
+  justify-content: center;
+  width: 23px;
+  color: ${e=>e.theme.isDarkMode?"#ccc":"#333"};
+
+  &::before {
+    content: 'v';
+  }
+`,w=(0,l.memo)(function({teams:e}){let[t,s]=e??[];return(0,n.jsxs)(o.Z,{children:[(0,n.jsx)(b,{team:t}),(0,n.jsx)(a.Z,{children:(0,n.jsx)(k,{})}),(0,n.jsx)(b,{team:s})]})}),y=(0,r.ZP)(i.Z)`
+  align-self: center;
+  width: auto;
+  max-width: initial;
+`,P=(0,l.forwardRef)(({matchups:e},t)=>(0,n.jsx)(y,{ref:t,children:(0,n.jsx)("tbody",{children:e?.map((e,t)=>n.jsx(w,{teams:e},t))})})),$=(0,l.memo)(P)},98514:(e,t,s)=>{s.d(t,{Z:()=>r});var n=s(70394);let l=n.ZP.div`
+  margin-top: 10px;
+  margin-bottom: 5px;
+  user-select: none;
+`,r=l}}]);
