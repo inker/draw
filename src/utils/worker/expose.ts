13dcc44ee8c89c8293f5ec6e323824b0e88b5761
Anton Veselev <anton.veselyev@gmail.com>
2024-01-25 02:09:24 +0000

    improve typing

--- src/utils/worker/expose.ts
+++ src/utils/worker/expose.ts
@@ -1,14 +1,26 @@
+import {
+  FOR_WORKER_CORRELATION_ID,
+  FOR_WORKER_DATA_KEY,
+  FROM_WORKER_CORRELATION_ID,
+  FROM_WORKER_DATA_KEY,
+  type MessageForWorker,
+  type MessageFromWorker,
+} from './constants'
+
 export default <F extends (arg: any) => any>(func: F) => {
   // eslint-disable-next-line no-restricted-globals
   addEventListener('message', (e: MessageEvent) => {
-    const { correlationId, data } = e.data
+    const {
+      [FOR_WORKER_CORRELATION_ID]: correlationId,
+      [FOR_WORKER_DATA_KEY]: data,
+    } = e.data as MessageForWorker<Parameters<typeof func>[0]>
 
     const result = func(data)
 
     postMessage({
-      correlationId,
-      data: result,
-    })
+      [FROM_WORKER_CORRELATION_ID]: correlationId,
+      [FROM_WORKER_DATA_KEY]: result,
+    } satisfies MessageFromWorker<typeof result>)
   })
 }
 
--- src/utils/worker/sendAndReceive.ts
+++ src/utils/worker/sendAndReceive.ts
@@ -1,30 +1,36 @@
-export default <Request, Response>(worker: Worker) => {
-  interface ReceivedMessage {
-    correlationId: string
-    data: Response
-  }
+import {
+  FOR_WORKER_CORRELATION_ID,
+  FOR_WORKER_DATA_KEY,
+  FROM_WORKER_CORRELATION_ID,
+  FROM_WORKER_DATA_KEY,
+  type MessageFromWorker,
+} from './constants'
 
+export default <Request, Response>(worker: Worker) => {
   type Callback = (response: Response) => void
   const callbacks = new Map<string, Callback>()
 
-  worker.addEventListener('message', (e: MessageEvent<ReceivedMessage>) => {
-    const id = e.data.correlationId
-    const cb = callbacks.get(e.data.correlationId)
-    if (!cb) {
-      return
-    }
+  worker.addEventListener(
+    'message',
+    (e: MessageEvent<MessageFromWorker<Response>>) => {
+      const id = e.data[FROM_WORKER_CORRELATION_ID]
+      const cb = callbacks.get(id)
+      if (!cb) {
+        return
+      }
 
-    callbacks.delete(id)
-    cb(e.data.data)
-  })
+      callbacks.delete(id)
+      cb(e.data[FROM_WORKER_DATA_KEY])
+    },
+  )
 
   return (message: Request) =>
     new Promise<Response>(resolve => {
       const id = globalThis.crypto.randomUUID()
       callbacks.set(id, resolve)
       worker.postMessage({
-        correlationId: id,
-        data: message,
+        [FOR_WORKER_CORRELATION_ID]: id,
+        [FOR_WORKER_DATA_KEY]: message,
       })
     })
 }
