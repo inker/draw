4780bfda9f65adc205defeafb8941c3e0942dcb4
Anton Veselev <anton.veselyev@gmail.com>
2024-02-04 18:52:44 +0000

    upd

--- src/utils/objectToFunction.ts
+++ src/utils/objectToFunction.ts
@@ -1,9 +1,15 @@
+interface PropertyAccessorFunc<T extends Record<string, unknown>> {
+  <K extends keyof T>(key: K): T[K];
+  (key: string | number): T[keyof T] | undefined;
+}
+
 /**
  * Makes an object callable,
  * i.e. converts it to a function, so one can use
  * round braces instead of square brackets
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export default <T extends {}>(o: T) =>
+export default <T extends Record<string, unknown>>(
+    o: T,
+  ): PropertyAccessorFunc<T> =>
   <K extends keyof T>(key: K) =>
     o[key];
