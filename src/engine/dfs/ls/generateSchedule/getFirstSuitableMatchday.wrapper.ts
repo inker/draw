3db2732f76375ba9dea3dc3c977bf844894579b8
Anton Veselev <anton.veselyev@gmail.com>
2024-07-08 23:13:19 +0100

    Revert "tune parameters"
    This reverts commit 0bd6ba386c815b0b53afddd9a925c8d1927ffaa4.

--- src/engine/dfs/ls/generateSchedule/getFirstSuitableMatchday.wrapper.ts
+++ src/engine/dfs/ls/generateSchedule/getFirstSuitableMatchday.wrapper.ts
@@ -83,8 +83,8 @@ export default ({
       };
     },
     getTimeout: workerIndex => {
-      const factor = workerIndex === 0 ? 100 : 7 / (workerIndex + 1);
-      return factor * 5000;
+      const factor = workerIndex === 0 ? 50 : 7 / (workerIndex + 1);
+      return factor * 10000;
     },
     signal,
   });
