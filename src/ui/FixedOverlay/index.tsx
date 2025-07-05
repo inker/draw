acfce00f58dcd206d88f4db971af0f4cc4b2a97e
Anton Veselev <anton.veselyev@gmail.com>
2025-07-05 15:46:48 +0100

    fixed overlay

--- src/ui/FixedOverlay/index.tsx
+++ src/ui/FixedOverlay/index.tsx
@@ -1,22 +1,11 @@
 import { type ReactNode, memo } from 'react';
 
-import makeStyleClass from '#utils/makeStyleClass';
 import Portal from '#ui/Portal';
 
-const airborneDivClass = makeStyleClass`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1000;
-  pointer-events: none;
-`;
+import * as styles from './styles.module.scss';
 
 const airborneDiv = document.createElement('div');
-airborneDiv.classList.add(airborneDivClass);
+airborneDiv.classList.add(styles.airborne);
 document.body.insertBefore(airborneDiv, document.getElementById('app'));
 
 interface Props {
