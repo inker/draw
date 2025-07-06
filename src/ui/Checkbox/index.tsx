3a7b99ef6c8c446c92d6e06afe3d5909373fab6b
Anton Veselev <anton.veselyev@gmail.com>
2025-07-06 10:24:35 +0100

    checkbox

--- src/ui/Checkbox/index.tsx
+++ src/ui/Checkbox/index.tsx
@@ -5,37 +5,32 @@ import {
   memo,
   useCallback,
 } from 'react';
-import styled, { css } from 'styled-components';
+import clsx from 'clsx';
+
+import useIsDarkMode from '#utils/hooks/useIsDarkMode';
 
 import * as styles from './styles.module.scss';
-
-const Box = styled.input.attrs({
-  type: 'checkbox',
-})`
-  cursor: pointer;
-  ${props =>
-    props.theme.isDarkMode &&
-    css`
-      border: var(--border);
-      accent-color: #246;
-
-      &:hover {
-        accent-color: #468;
-      }
-    `}
-`;
 
 type InputProps = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-interface Props extends Omit<InputProps, 'value' | 'onChange'> {
+interface Props extends Omit<InputProps, 'value' | 'onChange' | 'className'> {
   value: boolean;
+  checkboxClassName?: string;
   onChange: (value: boolean) => void;
 }
 
-function SelectWithHiddenLabel({ children, value, onChange, ...props }: Props) {
+function SelectWithHiddenLabel({
+  children,
+  value,
+  checkboxClassName,
+  onChange,
+  ...props
+}: Props) {
+  const isDarkMode = useIsDarkMode();
+
   const rawOnChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
       onChange(e.target.checked);
@@ -46,8 +41,14 @@ function SelectWithHiddenLabel({ children, value, onChange, ...props }: Props) {
   return (
     // eslint-disable-next-line jsx-a11y/label-has-associated-control
     <label className={styles.root}>
-      <Box
+      <input
+        type="checkbox"
         checked={value}
+        className={clsx(
+          styles.box,
+          isDarkMode && styles.dark,
+          checkboxClassName,
+        )}
         onChange={rawOnChange}
         {...(props as any)}
       />
