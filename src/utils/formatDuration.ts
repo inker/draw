5a78f6369ac6687fd7b82378211595976659cffe
Anton Veselev <anton.veselyev@gmail.com>
2025-08-09 08:46:20 +0100

    different format

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,24 +1,11 @@
 export default (ms: number) => {
   const totalSeconds = Math.floor(ms / 1000);
 
-  const days = Math.floor(totalSeconds / 86400);
-  const hours = Math.floor((totalSeconds % 86400) / 3600);
+  const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = totalSeconds % 60;
 
-  const parts: string[] = [];
-  if (days) {
-    parts.push(`${days} day${days === 1 ? '' : 's'}`);
-  }
-  if (hours) {
-    parts.push(`${hours} hour${hours === 1 ? '' : 's'}`);
-  }
-  if (minutes) {
-    parts.push(`${minutes} minute${minutes === 1 ? '' : 's'}`);
-  }
-  if (seconds || parts.length === 0) {
-    parts.push(`${seconds} second${seconds === 1 ? '' : 's'}`);
-  }
-
-  return parts.join(', ');
+  return [hours, minutes, seconds]
+    .map(part => part.toString().padStart(2, '0'))
+    .join(':');
 };
