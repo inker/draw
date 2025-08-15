c0bbcef509e1ce690a6aa18994f94dc7b98f14d2
Anton Veselev <anton.veselyev@gmail.com>
2025-08-15 22:25:15 +0100

    benfica & sporting

--- src/engine/dfs/ls/generateSchedule/teamsSharingStadium.ts
+++ src/engine/dfs/ls/generateSchedule/teamsSharingStadium.ts
@@ -2,4 +2,5 @@ export default [
   ['Internazionale', 'Milan'],
   ['Roma', 'Lazio'],
   ['APOEL', 'Omonia'],
+  ['Benfica', 'Sporting'],
 ] as const satisfies readonly (readonly [string, string])[];
