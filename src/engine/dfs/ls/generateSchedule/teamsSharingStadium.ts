f4317092a98e2162d22d1cc6bef76ac9e06382ba
Anton Veselev <anton.veselyev@gmail.com>
2025-08-16 12:10:20 +0100

    Revert "Reapply "revert""
    This reverts commit 60a7b40d8b6009ca8ab02385b2c58755ef8e5e70.

--- src/engine/dfs/ls/generateSchedule/teamsSharingStadium.ts
+++ src/engine/dfs/ls/generateSchedule/teamsSharingStadium.ts
@@ -2,4 +2,5 @@ export default [
   ['Internazionale', 'Milan'],
   ['Roma', 'Lazio'],
   ['APOEL', 'Omonia'],
+  ['Benfica', 'Sporting CP'],
 ] as const satisfies readonly (readonly [string, string])[];
