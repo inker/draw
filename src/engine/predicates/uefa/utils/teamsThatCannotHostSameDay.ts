1891af817c31d489449bb6b7329ba80106b43929
Anton Veselev <anton.veselyev@gmail.com>
2026-08-27 12:23:29 +0300

    upd

--- src/engine/predicates/uefa/utils/teamsThatCannotHostSameDay.ts
+++ src/engine/predicates/uefa/utils/teamsThatCannotHostSameDay.ts
@@ -3,4 +3,7 @@ export default [
   ['Roma', 'Lazio'],
   ['APOEL', 'Omonia'],
   ['Benfica', 'Sporting CP'],
+  ['Real Madrid', 'Atlético'],
+  ['Manchester City', 'Manchester United'],
+  ['Fenerbahçe', 'Galatasaray'],
 ] as const satisfies readonly (readonly [string, string])[];
