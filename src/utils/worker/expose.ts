c44956b59811057d26ece5e017ad4e80aac11366
Anton Veselev <anton.veselyev@gmail.com>
2026-09-26 11:35:48 +0100

    typings

--- src/utils/worker/expose.ts
+++ src/utils/worker/expose.ts
@@ -8,12 +8,16 @@ import {
 } from './constants';
 
 export default (func: (arg: any) => any) => {
+  type IncomingMessage = MessageForWorker<Parameters<typeof func>[0]>;
+
+  type ResponseMessage = MessageFromWorker<ReturnType<typeof func>>;
+
   // eslint-disable-next-line no-restricted-globals
-  addEventListener('message', (e: MessageEvent) => {
+  addEventListener('message', (e: MessageEvent<IncomingMessage>) => {
     const {
       [FOR_WORKER_CORRELATION_ID]: correlationId,
       [FOR_WORKER_DATA_KEY]: data,
-    } = e.data as MessageForWorker<Parameters<typeof func>[0]>;
+    } = e.data;
 
     try {
       const result = func(data);
@@ -22,13 +26,13 @@ export default (func: (arg: any) => any) => {
         type: 'result',
         [FROM_WORKER_CORRELATION_ID]: correlationId,
         [FROM_WORKER_DATA_KEY]: result,
-      } satisfies MessageFromWorker<ReturnType<typeof func>>);
+      } satisfies ResponseMessage);
     } catch (err) {
       postMessage({
         type: 'error',
         [FROM_WORKER_CORRELATION_ID]: correlationId,
         [FROM_WORKER_DATA_KEY]: err as Error,
-      } satisfies MessageFromWorker<ReturnType<typeof func>>);
+      } satisfies ResponseMessage);
     }
   });
 };
